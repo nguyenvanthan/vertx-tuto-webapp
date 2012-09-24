a342bec5d73d0626d37eb04c78c8ede9f56a70d9
Christian NGUYEN VAN THAN <christian.nguyenvanthan@gmail.com>
2012-09-24 23:37:32 +0200

    finish step 6. Hard fight with mongodb and virtual box :P

--- App.groovy
+++ App.groovy
@@ -1,8 +1,41 @@
 def webServerConf = [
+
+  // Normal web server stuff
   port: 8080,
-  host: 'localhost'
+  host: 'localhost',
+  
+  bridge: true,  // also act like an event bus bridge
+  
+  inbound_permitted: [ // allow messages from the client --> server
+    // Allow calls to get static album data from the persistor
+    [
+      address : 'vertx.mongopersistor',
+      match : [
+        action : 'find',
+        collection : 'albums'
+      ]
+    ]
+  ],
+  outbound_permitted: [ [:] ] // allow messages from the server --> client
+ 
 ]
+
+
+def mongodbConf = [
+	address: 'vertx.mongopersistor',
+	host: 'localhost',
+	port: 27017,
+	db_name: 'vertx_db'
+]	
 
 // Start the web server, with the config we defined above
 
-container.deployModule('vertx.web-server-v1.0', webServerConf);
+container.with {
+  // Deploy a MongoDB persistor module
+  deployModule('vertx.mongo-persistor-v1.0', mongodbConf, 1) { // add my mongodb configuration
+	deployVerticle('StaticData.groovy') // add static data
+  }
+
+  // Start the web server, with the config we defined above
+  deployModule('vertx.web-server-v1.0', webServerConf)
+}
