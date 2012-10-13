6992fd980e12e67ce63b51974ed03134369f2c90
Christian NGUYEN VAN THAN <christian.nguyenvanthan@gmail.com>
2012-10-13 21:37:56 +0200

    modif to step 10

--- App.groovy
+++ App.groovy
@@ -7,12 +7,26 @@ def webServerConf = [
   bridge: true,  // also act like an event bus bridge
   
   inbound_permitted: [ // allow messages from the client --> server
+  
     // Allow calls to get static album data from the persistor
     [
       address : 'vertx.mongopersistor',
       match : [
         action : 'find',
         collection : 'albums'
+      ]
+    ],
+	// Allow calls to login
+	[
+	   address: 'vertx.basicauthmanager.login'
+	],
+	// And to place orders
+    [
+      address : 'vertx.mongopersistor',
+      requires_auth : true,  // User must be logged in to send let these through
+      match : [
+        action : 'save',
+        collection : 'orders'
       ]
     ]
   ],
@@ -35,6 +49,9 @@ container.with {
   deployModule('vertx.mongo-persistor-v1.0', mongodbConf, 1) { // add my mongodb configuration
 	deployVerticle('StaticData.groovy') // add static data
   }
+  
+  // Deploy an auth manager to handle the authentication
+  deployModule('vertx.auth-mgr-v1.0')
 
   // Start the web server, with the config we defined above
   deployModule('vertx.web-server-v1.0', webServerConf)
